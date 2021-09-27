8cd13a11537e3a9763eb32637c3f5840e1389015
Prasanna Kommineni <prasanna.kommineni@ibm.com>
2021-09-27 15:38:54 +0530

    eureka server

---
+++ src/test/java/com/kps/curencyconv/EurekaCcApplicationTests.java
@@ -0,0 +1,13 @@
+//package com.kps.curencyconv;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class EurekaCcApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
